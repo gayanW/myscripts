4b50a04e4e93c2233272440de62c8e04b2936c3c
Gayan Weerakutti <reversiblean@live.com>
2015-09-13 21:35:20 +0530

    Ubuntu Post Installation Script
    One click install script to automate: Thing to do after installing
    Ubuntu

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ubuntu restricted extras
+sudo apt-get install ubuntu-restricted-extras
+
+# Oracle Java
+sudo add-apt-repositroy ppa:webup8team/java
+sudo apt-get install oracle-java8-installer
+
+# Android Studio
+umake android android-studio
